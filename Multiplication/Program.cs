c8bb7f57185894ae0318843acb13e713fc913219
LBKKBANCHAS\bancha.s <bancha.s@aware.co.th>
2021-03-03 11:49:30 +0700

    Code Commit
    Code Commit

---
+++ Multiplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Hello World!");
+            Console.Write("Input: N = ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();  //อ่านค่าที่รับมา
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(num + "x" + i + "=" + num * i);
+            }
+
+        }
+    
+    }
+}
